5d0dc732c0bc3a858e637fced7843175cbfd2365
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2024-09-17 20:22:53 +0500

    Keepers App

---
+++ src/Components/CreateArea.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [title, setTitle] = useState("");
+  const [notedes, setNoteDes] = useState("");
+
+  function titleValue() {
+    const newValue = event.target.value;
+    setTitle(newValue);
+    console.log(title);
+  }
+
+  function noteValue() {
+    const newValue = event.target.value;
+    setNoteDes(newValue);
+    console.log(notedes);
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={titleValue}
+          name="title"
+          placeholder="Title"
+          value={title}
+        />
+        <textarea
+          onChange={noteValue}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={notedes}
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
